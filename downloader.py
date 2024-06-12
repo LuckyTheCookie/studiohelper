91f47a082c61c2d37d4a80a369ab25afac7ff9af
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2024-06-12 22:53:10 +0200

    Optimisation ✨

--- downloader.py
+++ downloader.py
@@ -1,3 +1,5 @@
+# downloader.py
+
 import os
 import requests
 import zipfile
@@ -15,68 +17,56 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+def display_progress(progress, total):
+    percent = int(progress / total * 100)
+    bar = f"{percent}% [{'#' * (percent // 2)}{' ' * (50 - percent // 2)}]"
+    print(f"\r{bar}", end='')
+
 def download_and_unzip_github_release():
-    print("")
-    print("Il semble que vous utilisiez une ancienne version de STUdio.")
-    print("Nous vous recommandons FORTEMENT de mettre à jour STUdio pour résoudre les problèmes que vous rencontrez.")
-    print("STUdio va s'installer automatiquement pour vous.")
-    print("")
+    print("\nIl semble que vous utilisiez une ancienne version de STUdio.\n"
+          "Nous vous recommandons FORTEMENT de mettre à jour STUdio pour résoudre les problèmes que vous rencontrez.\n"
+          "STUdio va s'installer automatiquement pour vous.\n")
     os.system('pause')
+
     version = get_studio_latest_version()
     if "Erreur" in version:
-        return  # Arrêter l'exécution si une erreur s'est produite lors de la récupération de la version
-    
+        return
+
     url = f"https://github.com/DantSu/studio/releases/download/{version}/studio-web-ui-{version}-dist.zip"
     local_filename = f"studio-web-ui-{version}-dist.zip"
 
-    # Fonction pour afficher une animation de pourcentage
-    def display_progress(progress, total):
-        percent = int(progress / total * 100)
-        bar = f"{percent}% [{'#' * (percent // 2)}{' ' * (50 - percent // 2)}]"
-        print(f"\r{bar}", end='')
-
-    # Téléchargement du fichier avec affichage de la progression
     with requests.get(url, stream=True) as response:
         if response.status_code == 200:
-            total_length = response.headers.get('content-length')
-            if total_length is None:  # Pas de contenu
-                print("Erreur : Contenu introuvable.")
-                return
-            
-            total_length = int(total_length)
+            total_length = int(response.headers.get('content-length', 0))
             downloaded = 0
-            
+
             with open(local_filename, 'wb') as f:
                 for chunk in response.iter_content(chunk_size=8192):
                     downloaded += len(chunk)
                     f.write(chunk)
                     display_progress(downloaded, total_length)
-            
+
             print(f"\nTéléchargement terminé : {local_filename}")
         else:
             print(f"Erreur de téléchargement : {response.status_code}")
             return
 
-    # Décompression du fichier ZIP avec affichage de la progression
     with zipfile.ZipFile(local_filename, 'r') as zip_ref:
         extract_dir = f"studio-web-ui-{version}"
         zip_ref.extractall(extract_dir)
-        
+
         total_files = len(zip_ref.infolist())
         for i, file in enumerate(zip_ref.infolist()):
             display_progress(i + 1, total_files)
-        
+
         print(f"\nDécompression terminée : {extract_dir}")
 
-    # Déplacer les fichiers du sous-dossier vers le dossier parent et supprimer le sous-dossier
     inner_dir = os.path.join(extract_dir, os.listdir(extract_dir)[0])
     for filename in os.listdir(inner_dir):
         shutil.move(os.path.join(inner_dir, filename), extract_dir)
-    
+
     os.rmdir(inner_dir)
     print(f"Dossier imbriqué supprimé : {inner_dir}")
 
-    # Suppression du fichier ZIP après extraction
     os.remove(local_filename)
     print(f"Fichier ZIP supprimé : {local_filename}")
-
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# Importation des modules
 import os
-
-from get_infos import get_system_info, get_java_info, get_studio_latest_log, get_studio_local_version, get_studio_latest_version
+from get_infos import (
+    get_system_info,
+    get_java_info,
+    get_studio_latest_log,
+    get_studio_local_version,
+    get_studio_latest_version,
+)
 from downloader import download_and_unzip_github_release
 
 class bcolors:
@@ -15,9 +19,6 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-print(f"{bcolors.WARNING}WARNING : CE PROGRAMME EST EN COURS DE DÉVELOPPEMENT ET PEUT NE PAS FONCTIONNER CORRECTEMENT.{bcolors.ENDC}")
-
-# Fonction pour créer le fichier contenant les informations
 def create_info_file():
     with open("full-log.txt", "w") as file:
         file.write(get_system_info())
@@ -28,104 +29,70 @@ def create_info_file():
         file.write("==============================\n")
         file.write(get_studio_latest_log())
 
-
 def logger():
-    print("")
-    print("Ce programme va récupérer des informations sur différentes informations afin de mieux vous aider.")
-    print("Ces informations serons collectées : Version de Java, Information du système d'exploitation, version de STUdio et le fichier studio-latest.log.")
-    print("Voulez-vous continuer? (Y/N)")
-    print("")
-    user_input = input()
-    if user_input.lower() == "y":
+    print("\nCe programme va récupérer des informations sur différentes informations afin de mieux vous aider.\n"
+          "Ces informations serons collectées : Version de Java, Information du système d'exploitation, version de STUdio et le fichier studio-latest.log.\n"
+          "Voulez-vous continuer? (Y/N)\n")
+    if input().strip().lower() == "y":
         try:
             create_info_file()
-            print("")
-            print("\033[92m" + "Nous avons récupéré les informations avec succès. Les informations ont été stockées dans le fichier full-log.txt.")
-            print("Veuillez envoyer ce fichier sur discord pour obtenir de l'aide.")
-            # Copier le fichier dans le presse-papiers
+            print(f"\n{bcolors.OKGREEN}Nous avons récupéré les informations avec succès. Les informations ont été stockées dans le fichier full-log.txt.\n"
+                  "Veuillez envoyer ce fichier sur discord pour obtenir de l'aide.")
             os.system("clip < full-log.txt")
             print(f"{bcolors.OKCYAN}Le fichier a été copié dans le presse-papiers, vous pouvez le coller directement dans discord.")
             os.system('pause')
-            exit()
         except Exception as e:
-            print("Erreur: " + str(e))
-            print(f"{bcolors.FAIL}Une erreur s'est produite lors de la récupération des informations.{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}Veuillez suivre les instructions ci-dessous pour résoudre le problème:{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}1. Assurez-vous que vous avez les permissions nécessaires pour accéder aux fichiers et dossiers. Si vous êtes sur Windows, exécutez le programme en tant qu'administrateur.{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}2. Assurez-vous que le fichier studio-latest.log est présent dans le même répertoire que le programme{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}3. Assurez-vous que Java est installé et configuré correctement sur votre système.{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}Si vous rencontrez toujours des problèmes, veuillez contacter un modérateur pour obtenir de l'aide.{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Une erreur s'est produite lors de la récupération des informations: {str(e)}\n"
+                  "1. Assurez-vous que vous avez les permissions nécessaires pour accéder aux fichiers et dossiers. "
+                  "Si vous êtes sur Windows, exécutez le programme en tant qu'administrateur.\n"
+                  "2. Assurez-vous que le fichier studio-latest.log est présent dans le même répertoire que le programme.\n"
+                  "3. Assurez-vous que Java est installé et configuré correctement sur votre système.\n"
+                  "Si vous rencontrez toujours des problèmes, veuillez contacter un modérateur pour obtenir de l'aide.")
     else:
         print("Le programme va se fermer...")
 
 def solver():
-    print("")
-    print("Il semble que vous ayez un problème avec STUdio.")
-    print("Je suis là pour vous aider !")
-    print("")
-    print("Pour commencer, j'ai besoin de quelques informations sur votre système d'exploitation et votre compte utilisateur afin de mieux comprendre le problème.")
-    print("Ces informations ne quittent pas votre ordinateur")
-    print("Voulez-vous continuer? (Y/N)")
-    print("")
-
-    user_input = input()
-    if user_input.lower() == "y":
-        print("")
-        print("Je vais maintenant vérifier si vous utilisez la dernière version de STUdio.")
-        if get_studio_local_version() == "DantSu STUdio " + get_studio_latest_version() + "\n":
-            print("\033[92m" + "Vous semblez utiliser la dernière version de STUdio.")
+    print("\nIl semble que vous ayez un problème avec STUdio. Je suis là pour vous aider !\n"
+          "Pour commencer, j'ai besoin de quelques informations sur votre système d'exploitation et votre compte utilisateur afin de mieux comprendre le problème.\n"
+          "Ces informations ne quittent pas votre ordinateur\nVoulez-vous continuer? (Y/N)\n")
+    if input().strip().lower() == "y":
+        if get_studio_local_version() == f"DantSu STUdio {get_studio_latest_version()}\n":
+            print(f"{bcolors.OKGREEN}Vous semblez utiliser la dernière version de STUdio.")
         else:
             download_and_unzip_github_release()
-            print("")
-            print(f"{bcolors.OKGREEN}STUdio a été mis à jour avec succès !{bcolors.ENDC}")
-            print(f"{bcolors.WARNING}Cependant, le travail n'est pas encore fini !")
-            print(f"Veuillez fermer ce programme et le deplacer dans le dossier contenant la nouvelle version de STUdio.")
-            print(f"Le dossier contenant la nouvelle version de STUdio est nommé studio-web-ui-{get_studio_latest_version()}.")
-            print(f"Ensuite, exécutez à nouveau ce programme pour obtenir de l'aide.{bcolors.ENDC}")
+            print(f"{bcolors.OKGREEN}STUdio a été mis à jour avec succès !\n"
+                  f"{bcolors.WARNING}Veuillez fermer ce programme et le déplacer dans le dossier contenant la nouvelle version de STUdio.\n"
+                  f"Le dossier contenant la nouvelle version de STUdio est nommé studio-web-ui-{get_studio_latest_version()}.\n"
+                  "Ensuite, exécutez à nouveau ce programme pour obtenir de l'aide.")
             os.system('pause')
-            print("")
             exit()
-        
 
-        print("Je vais vérifier si Java est installé et configuré correctement sur votre système.")
         if "Erreur" in get_java_info():
-            print(f"{bcolors.FAIL}Il semble que Java ne soit pas installé ou configuré correctement.{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}Veuillez installer Java et assurez-vous qu'il est configuré correctement.{bcolors.ENDC}")
-            print(f"{bcolors.FAIL}Vous pouvez télécharger Java à partir du site officiel: https://www.oracle.com/java/technologies/downloads/{bcolors.ENDC}")
+            print(f"{bcolors.FAIL}Il semble que Java ne soit pas installé ou configuré correctement.\n"
+                  "Veuillez installer Java et assurez-vous qu'il est configuré correctement.\n"
+                  "Vous pouvez télécharger Java à partir du site officiel: https://www.oracle.com/java/technologies/downloads/")
         else:
-            print("")
-            print(f"{bcolors.OKGREEN}Java semble correctement installé sur votre ordinateur{bcolors.ENDC}")
-            print("")
-
+            print(f"{bcolors.OKGREEN}Java semble correctement installé sur votre ordinateur{bcolors.ENDC}\n")
         os.system('pause')
 
-
-        print("Je vous conseille de demander de l'aide à un modérateur pour résoudre votre problème.")
-        print("Voulez-vous que je récupère les informations nécessaires pour vous afin de faciliter l'aide apportée par un modérateur? (Y/N)")
-        print("")
-        user_input = input()
-        if user_input.lower() == "y":
+        print("Voulez-vous que je récupère les informations nécessaires pour vous afin de faciliter l'aide apportée par un modérateur? (Y/N)\n")
+        if input().strip().lower() == "y":
             logger()
         else:
-            print("Je n'ai pas pu trouver de solution à votre problème...")
-            print("Veuillez contacter un modérateur pour obtenir de l'aide.")
-            print("Il est cependement FORTEMENT recommandé de récupérer les informations nécessaires pour faciliter l'aide apportée par un modérateur.")
-            print("Le programme va se fermer...")
+            print("Veuillez contacter un modérateur pour obtenir de l'aide.\n"
+                  "Le programme va se fermer...")
             exit()
     else:
         print("Le programme va se fermer... Aucune information n'a été collectée.")
         exit()
 
-
 def main():
-    print("Bienvenue dans le programme de récupération d'informations sur STUdio.")
-    print("Veuillez choisir l'option qui correspond à votre situation:")
-    print("1. Vous avez un problème avec STUdio et vous avez besoin d'aide.")
-    print("2. Un modérateur vous a demandé de récupérer le fichier log pour obtenir de l'aide.")
-    print("3. Quitter le programme.")
-    print("")
-
-    user_input = input()
+    print("Bienvenue dans le programme de récupération d'informations sur STUdio.\n"
+          "Veuillez choisir l'option qui correspond à votre situation:\n"
+          "1. Vous avez un problème avec STUdio et vous avez besoin d'aide.\n"
+          "2. Un modérateur vous a demandé de récupérer le fichier log pour obtenir de l'aide.\n"
+          "3. Quitter le programme.\n")
+    user_input = input().strip()
     if user_input == "1":
         solver()
     elif user_input == "2":
