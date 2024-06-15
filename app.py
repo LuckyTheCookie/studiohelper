ebfe19d09c8e1964fa323ff661ed8db1f19df224
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2024-06-15 16:54:43 +0200

    Updated App.py 🧪

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template, request, jsonify
 import os
 from get_infos import get_system_info, get_java_info, get_studio_latest_log, get_studio_local_version, get_studio_latest_version
 from downloader import download_and_unzip_github_release
+import webbrowser
 
 app = Flask(__name__)
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route('/create_info_file', methods=['POST'])
 def create_info_file_route():
@@ -44,4 +57,16 @@ def create_info_file():
         file.write(get_studio_latest_log())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print(f"{bcolors.OKCYAN}███████╗████████╗██╗   ██╗██████╗ ██╗ ██████╗     ██╗  ██╗███████╗██╗     ██████╗ ███████╗██████╗ \n\
+██╔════╝╚══██╔══╝██║   ██║██╔══██╗██║██╔═══██╗    ██║  ██║██╔════╝██║     ██╔══██╗██╔════╝██╔══██╗\n\
+███████╗   ██║   ██║   ██║██║  ██║██║██║   ██║    ███████║█████╗  ██║     ██████╔╝█████╗  ██████╔╝\n\
+╚════██║   ██║   ██║   ██║██║  ██║██║██║   ██║    ██╔══██║██╔══╝  ██║     ██╔═══╝ ██╔══╝  ██╔══██╗\n\
+███████║   ██║   ╚██████╔╝██████╔╝██║╚██████╔╝    ██║  ██║███████╗███████╗██║     ███████╗██║  ██║\n\
+╚══════╝   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝ ╚═════╝     ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝{bcolors.ENDC}")
+    
+    print(f"{bcolors.OKGREEN}Server started on http://localhost:5241{bcolors.ENDC}")
+    webbrowser.open("http://localhost:5241")
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5241)    
+    
+    
