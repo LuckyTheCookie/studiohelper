6ac6f910012daa1a55c7e6a15821e2c200070500
Lucky <85549951+LuckyTheCookie@users.noreply.github.com>
2024-06-16 22:24:09 +0200

    Post method updated 📫

--- static/scripts/post.js
+++ static/scripts/post.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     $('#createInfoFile').click(function() {
         $.post('/create_info_file', function(data) {
             $('#result').text(data.message);
@@ -16,4 +16,45 @@ $(document).ready(function() {
             $('#result').text(data.message);
         });
     });
+
+    $('#goToSolver').click(function() {
+        window.location.href = '/solver';
+    });
+
+    $('#solverAccept').click(function() {
+        $.post('/solver_accept', function(data) {
+            $('#result').text(data.message);
+            if (data.status === 'success') {
+                $('#solverWaitingStep').hide();
+                $('#solverSuccessStep').show();
+            } else if (data.status === 'error') {
+                $('#solverWaitingStep').hide();
+                $('#solverErrorStep').show();
+            } else if (data.status === 'reload') {
+                $('#solverWaitingStep').hide();
+                $('#solverReloadStep').show();
+            }
+        });
+        $('#solverFirstStep').hide();
+        $('#solverWaitingStep').show();
+        
+    });
+
+    $('#mainMenuButton').click(function() {
+        window.location.href = '/';
+    });
+
+    $('#solverClose').click(function() {
+        $('#solverCloseCategory').hide();
+        $('#solverClosed').show();
+        $.post('/solver_close', function(data) {
+            $('#result').text(data.message);
+        });
+    });
+
+    $('#solverRetry').click(function() {
+        // Recharger la page
+        window.location.reload();
+    }
+    );
 });
